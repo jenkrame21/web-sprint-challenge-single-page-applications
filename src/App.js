ce9fe7bbb5017b0fa6537cd1dd06fa36dcd98dd3
Jennifer Kramer <jenkrame@gmail.com>
2020-10-16 11:41:44 -0400

    Created PizzaForm and Pizza files and linked them appropriately.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import PizzaForm from './PizzaForm';
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <header>
+        <h1>Lambda Eats</h1>
+        <p>You can remove this code and create your own header</p>
+      </header>
+      <nav>
+        <PizzaForm />
+      </nav>
     </>
   );
 };
---
+++ src/PizzaForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Pizza from './Pizza';
+
+export default function PizzaForm() {
+
+
+
+
+    return (
+        <form>
+
+        </form>
+    )
+
+}
+
